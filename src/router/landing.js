79fe46dbe067fc08b993d1415dcc198f7b44791d
Freeman-md <freemanmadudili@gmail.com>
2021-10-20 12:08:43 +0100

    Update landing.js

--- src/router/landing.js
+++ src/router/landing.js
@@ -1,15 +1,16 @@
 import Home from '@/views/Home.vue'
-import AboutUs from '@/views/AboutUs.vue'
-import Products from '@/views/Products.vue'
 import SocialImpact from '@/views/SocialImpact.vue'
-import Blog from '@/views/Blog.vue'
-import Shop from '@/views/Shop.vue'
-import ContactUs from '@/views/ContactUs.vue'
 import FAQ from '@/views/FAQ.vue'
 import PrivacyPolicy from '@/views/PrivacyPolicy.vue'
 import TermsOfService from '@/views/TermsOfService.vue'
-import BlogPost from '@/views/BlogPost.vue'
 import NotFound from '@/views/NotFound.vue'
+
+// import AboutUs from '@/views/AboutUs.vue'
+// import Products from '@/views/Products.vue'
+// import Blog from '@/views/Blog.vue'
+// import Shop from '@/views/Shop.vue'
+// import ContactUs from '@/views/ContactUs.vue'
+// import BlogPost from '@/views/BlogPost.vue'
 
 export default [
   {
@@ -18,34 +19,39 @@ export default [
     component: Home
   },
   {
-    path: '/about-us',
-    name: 'About Us',
-    component: AboutUs
+    path: '/local-market',
+    name: 'Local Market',
+    component: LocalMarket
   },
   {
-    path: '/products',
-    name: 'Products',
-    component: Products
+    path: '/global-market',
+    name: 'Global Market',
+    component: GlobalMarket
+  },
+  {
+    path: '/product-estimator',
+    name: 'ProductEstimator',
+    component: ProductEstimator
+  },
+  {
+    path: '/our-company',
+    name: 'Our Company',
+    component: OurCompany
+  },
+  {
+    path: '/our-team',
+    name: 'Our Team',
+    component: OurTeam
+  },
+  {
+    path: '/our-core-values',
+    name: 'Our CoreValues',
+    component: OurCoreValues
   },
   {
     path: '/social-impact',
     name: 'Social Impact',
     component: SocialImpact
-  },
-  {
-    path: '/blog',
-    name: 'Blog',
-    component: Blog
-  },
-  {
-    path: '/shop',
-    name: 'Shop',
-    component: Shop
-  },
-  {
-    path: '/contact-us',
-    name: 'Contact Us',
-    component: ContactUs
   },
   {
     path: '/frequently-asked-questions',
@@ -63,13 +69,38 @@ export default [
     component: TermsOfService
   },
   {
-    path: '/:slug',
-    name: 'Blog Post',
-    component: BlogPost
-  },
-  {
     path: '/*',
     name: '404',
     component: NotFound
-  }
+  },
+  // {
+  //   path: '/about-us',
+  //   name: 'About Us',
+  //   component: AboutUs
+  // },
+  // {
+  //   path: '/products',
+  //   name: 'Products',
+  //   component: Products
+  // },
+  // {
+  //   path: '/blog',
+  //   name: 'Blog',
+  //   component: Blog
+  // },
+  // {
+  //   path: '/shop',
+  //   name: 'Shop',
+  //   component: Shop
+  // },
+  // {
+  //   path: '/contact-us',
+  //   name: 'Contact Us',
+  //   component: ContactUs
+  // },
+  // {
+  //   path: '/:slug',
+  //   name: 'Blog Post',
+  //   component: BlogPost
+  // },
 ]
